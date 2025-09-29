f377c0f49a8c834dcec4ba081caba3b0a95c60b2
songgot <kr2068858@naver.com>
2025-09-29 20:38:52 +0900

    yt-dlp cookies-from-browser 옵션 추가

--- src/app/api/fetch-youtube-video/route.ts
+++ src/app/api/fetch-youtube-video/route.ts
@@ -9,8 +9,8 @@ export async function POST(req:NextRequest){
                 "-f", "best", 
                 "-o", "-", url, 
                 "--no-warnings", 
-                "--user-agent", 
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+                "--cookies-from-browser",
+                "chrome"
             ]);
             const chunks: Buffer[] = [];
             const errChunks: Buffer[] = [];
